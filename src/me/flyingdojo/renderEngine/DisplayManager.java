5d2e6e455796b3bacc79081782f9c64b6b276fa5
flying-dojo <87005812+flying-dojo@users.noreply.github.com>
2021-07-23 18:55:20 +0300

    Finish DisplayManager

--- src/me/flyingdojo/renderEngine/DisplayManager.java
+++ src/me/flyingdojo/renderEngine/DisplayManager.java
@@ -1,26 +1,36 @@
 package me.flyingdojo.renderEngine;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class DisplayManager {
 	private static final int WIDTH = 1280;
 	private static final int HEIGHT = 720;
+	private static final int FPS_CAP = 120;
 
 	public static void createDisplay() {
+		ContextAttribs attribs = new ContextAttribs(3,2);
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+		
 		try {
-			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT)); // Set display width and height
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
+		
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
 	}
 
 	public static void updateDisplay() {
-
+		Display.sync(FPS_CAP); // Sync the display
+		Display.update(); // Update the display
 	}
 
 	public static void closeDisplay() {
-
+		Display.destroy(); // Destroy the display
 	}
 }
